7ef604a81bb671fe05d4755c7e05691b83afa224
Sampath Kasina <sampath.kasina@gmail.com>
2024-07-28 11:29:46 -0400

    first commit

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,156 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+
+/**
+ * The VM is configured to automatically run this class, and to call the functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the name of this class or
+ * the package after creating this project, you must also update the build.gradle file in the
+ * project.
+ */
+public class Robot extends TimedRobot {
+  // private static final String kDefaultAuto = "Default";
+  // private static final String kCustomAuto = "My Auto";
+  // private String m_autoSelected;
+  // private final SendableChooser<String> m_chooser = new SendableChooser<>();
+  // private Drivetrain drivetrain_ = new Drivetrain();
+  // private CommandXboxController controller_ = new CommandXboxController(0);
+
+  private DifferentialDrive m_myRobot;
+  private CommandXboxController controller_;
+  private CANSparkMax left_leader;
+  private CANSparkMax left_follower1;
+  private CANSparkMax left_follower2;
+  private CANSparkMax right_leader;
+  private CANSparkMax right_follower1;
+  private CANSparkMax right_follower2;
+
+  /**
+   * This function is run when the robot is first started up and should be used for any
+   * initialization code.
+   */
+  @Override
+  public void robotInit() {
+    // m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
+    // m_chooser.addOption("My Auto", kCustomAuto);
+    // SmartDashboard.putData("Auto choices", m_chooser);
+
+
+    // CAN ID should be configured using REV Hardware Client
+    left_leader = new CANSparkMax(0, MotorType.kBrushless);
+    left_leader.restoreFactoryDefaults();
+
+    left_follower1 = new CANSparkMax(1, MotorType.kBrushless);
+    left_follower1.follow(left_leader);
+    left_follower1.restoreFactoryDefaults();
+
+    left_follower2 = new CANSparkMax(2, MotorType.kBrushless);
+    left_follower2.follow(left_leader);
+    left_follower2.restoreFactoryDefaults();
+
+    right_leader = new CANSparkMax(3, MotorType.kBrushless);
+    right_leader.restoreFactoryDefaults();
+
+    right_follower1 = new CANSparkMax(4, MotorType.kBrushless);
+    right_follower1.follow(right_leader);
+    right_follower1.restoreFactoryDefaults();
+
+    right_follower2 = new CANSparkMax(5, MotorType.kBrushless);
+    right_follower2.follow(right_leader);
+    right_follower2.restoreFactoryDefaults();
+
+    m_myRobot = new DifferentialDrive(left_leader, right_leader);
+
+    controller_ = new CommandXboxController(0);
+    
+
+  }
+
+  /**
+   * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
+   * that you want ran during disabled, autonomous, teleoperated and test.
+   *
+   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
+   * SmartDashboard integrated updating.
+   */
+  @Override
+  public void robotPeriodic() {
+    //m_myRobot.tankDrive(controller_.getLeftY(), controller_.getRightY());
+    m_myRobot.arcadeDrive(controller_.getLeftY(), controller_.getRightX());
+
+  }
+
+  /**
+   * This autonomous (along with the chooser code above) shows how to select between different
+   * autonomous modes using the dashboard. The sendable chooser code works with the Java
+   * SmartDashboard. If you prefer the LabVIEW Dashboard, remove all of the chooser code and
+   * uncomment the getString line to get the auto name from the text box below the Gyro
+   *
+   * <p>You can add additional auto modes by adding additional comparisons to the switch structure
+   * below with additional strings. If using the SendableChooser make sure to add them to the
+   * chooser code above as well.
+   */
+  @Override
+  public void autonomousInit() {
+    // m_autoSelected = m_chooser.getSelected();
+    // // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
+    // System.out.println("Auto selected: " + m_autoSelected);
+  }
+
+  /** This function is called periodically during autonomous. */
+  @Override
+  public void autonomousPeriodic() {
+    // switch (m_autoSelected) {
+    //   case kCustomAuto:
+    //     // Put custom auto code here
+    //     break;
+    //   case kDefaultAuto:
+    //   default:
+    //     // Put default auto code here
+    //     break;
+    // }
+  }
+
+  /** This function is called once when teleop is enabled. */
+  @Override
+  public void teleopInit() {}
+
+  /** This function is called periodically during operator control. */
+  @Override
+  public void teleopPeriodic() {}
+
+  /** This function is called once when the robot is disabled. */
+  @Override
+  public void disabledInit() {}
+
+  /** This function is called periodically when disabled. */
+  @Override
+  public void disabledPeriodic() {}
+
+  /** This function is called once when test mode is enabled. */
+  @Override
+  public void testInit() {}
+
+  /** This function is called periodically during test mode. */
+  @Override
+  public void testPeriodic() {}
+
+  /** This function is called once when the robot is first started up. */
+  @Override
+  public void simulationInit() {}
+
+  /** This function is called periodically whilst in simulation. */
+  @Override
+  public void simulationPeriodic() {}
+}
